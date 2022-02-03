0f6d89ae88ea0687005e688dbc5ffa5fd264dafd
ekmacmini43 <ekmacmini43@ekmacmini43s-Mac-mini.local>
2022-02-03 19:25:42 +0545

    Nav bar configuration done

--- WeatherApp/AppDelegate.swift
+++ WeatherApp/AppDelegate.swift
@@ -11,7 +11,16 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        configureNavBar()
         return true
+    }
+    
+    private func configureNavBar() {
+        let appearance = UINavigationBar.appearance()
+        appearance.barTintColor = .secondary
+        appearance.tintColor = .white
+        appearance.titleTextAttributes = [NSAttributedString.Key.font: CustomFont.regular.of(.heading1),
+                                          NSAttributedString.Key.foregroundColor: UIColor.white]
     }
 
 }
